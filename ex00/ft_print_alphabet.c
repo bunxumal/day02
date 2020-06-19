f08da743110dd215145dd4b88b4d0352e4f49dce
bunxumal <66954951+bunxumal@users.noreply.github.com>
2020-06-19 12:02:11 +0200

    Add files via upload

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bunxumal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/19 11:31:59 by bunxumal          #+#    #+#             */
+/*   Updated: 2020/06/19 11:59:28 by bunxumal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+int ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+void ft_print_alphabet(void)  
+{
+	char letter;
+	letter = 'a';
+	while (letter <= 'z') {
+		ft_putchar (letter);
+		letter ++;
+	}
+}
+int main (void)
+{
+	ft_print_alphabet();
+	return (o);
+}
